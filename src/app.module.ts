3c53d56c67750bb46b0778f4522b4c18483d42a4
Maurice Wipf <maurice@gmx.de>
2020-08-27 08:18:11 +0200

    Import HttpModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
-import { Module } from '@nestjs/common';
+import { HttpModule, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CountryService } from './country/country.service';
 import { CountryController } from './country/country.controller';
 
 @Module({
-  imports: [],
+  imports: [
+    HttpModule,
+  ],
   controllers: [AppController, CountryController],
   providers: [AppService, CountryService],
 })
